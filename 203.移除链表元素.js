fbd820cb8eaea6390317c42b2479b7d4c406d176
lizhenxue <lizhenxue@100tal.com>
2020-08-21 10:28:02 +0800

    链表

--- 203.移除链表元素.js
+++ 203.移除链表元素.js
@@ -17,33 +17,33 @@
  * @param {number} val
  * @return {ListNode}
  */
-var removeElements = function(head, val) {
-  let cur = head,
-      pre = null;
-  while (cur) {
-    if (cur == head && cur.val === val) {
-      head = head.next
-      cur = head
-    }else{
-      cur.val === val?pre.next = cur.next : pre = cur
-      cur = cur.next;
-    }
-  }
-  return head;
-};
-// @lc code=end
-
+// var removeElements = function(head, val) {
+//   let cur = head,
+//       pre = null;
+//   while (cur) {
+//     if (cur == head && cur.val === val) {
+//       head = head.next
+//       cur = head
+//     }else{
+//       cur.val === val?pre.next = cur.next : pre = cur
+//       cur = cur.next;
+//     }
+//   }
+//   return head;
+// };
 var removeElements = function(head, val) {
   // a=>b=>c=>d=>e=>f
   // 这时候设置一个哨兵元素，放到第一个，只作为辅助使用，目的是少做第head的判断逻辑
-  let ele = {next: head}
-  let cur = ele,
-  while(cur.next) {
-    if ( cur.next.val === val) {
-      cur.next = cur.next.next
-    }else{
-      cur = cur.next;
-    }
+  let prev = new ListNode();
+  prev.next = head;
+  let curr = prev;
+  while(curr.next !== null) {
+      if(curr.next.val === val) {
+          curr.next = curr.next.next;
+      } else {
+          curr = curr.next;
+      }
   }
-  return ele.next;
+  return prev.next;
 };
+// @lc code=end
