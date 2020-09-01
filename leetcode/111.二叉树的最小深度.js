3b11bc5979a153646937bba83ef973d220883d1d
suixuejiqiren <1214587109@qq.com>
2020-09-01 10:58:53 +0800

    二叉树-简单4

--- leetcode/111.二叉树的最小深度.js
+++ leetcode/111.二叉树的最小深度.js
@@ -27,7 +27,7 @@ var minDepth1 = function(root) {
       for(const v of level) {
           const node = stack.shift()
           if(node.left == null && node.right == null) {
-              return deepNum
+            return deepNum
           }
           if(node.left) stack.push(node.left)
           if(node.right) stack.push(node.right)
@@ -37,7 +37,18 @@ var minDepth1 = function(root) {
   return deepNum
 };
 
-// function getMin()
-// var minDepth = function(root) {}
+
+var minDepth = function(root) {
+    if (root == null) return 0
+    if (root.left&&root.right){
+        return 1+Math.min(minDepth(root.left),minDepth(root.right))
+    }else if(root.left){
+        return 1+minDepth(root.left)
+    }else if(root.right){
+        return 1+minDepth(root.right)
+    }else{
+        return 1
+    }
+}
 // @lc code=end
 
