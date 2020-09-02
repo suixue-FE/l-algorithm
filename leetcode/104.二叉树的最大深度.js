a60a77e9cd013e05ac9012f91266007080e063ff
suixuejiqiren <1214587109@qq.com>
2020-09-02 10:21:13 +0800

    二叉树5？

---
+++ leetcode/104.二叉树的最大深度.js
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode.cn id=104 lang=javascript
+ *
+ * [104] 二叉树的最大深度
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth1 = function(root) {
+  if (root === null) return 0
+  // if (!root.left && !root.right) return 1
+  return 1 + Math.max(maxDepth1(root.left),maxDepth1(root.right))
+};
+
+var maxDepth = function(root) {
+  if (root === null) return 0
+  let stack = [root]
+  let length = 0
+  while (stack.length){
+    const len = stack.length
+    for (let i = 0; i < len; i++) {
+     let res = stack.shift()
+     if (res.left) stack.push(res.left)
+     if (res.right) stack.push(res.right)
+    }
+    length++
+  }
+  return length
+};
+// @lc code=end
+
---
+++ leetcode/236.二叉树的最近公共祖先.js
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode.cn id=236 lang=javascript
+ *
+ * [236] 二叉树的最近公共祖先
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+var lowestCommonAncestor = function(root, p, q) {
+    // 这个完全没想出来，看了大佬的半天，试着写一下
+    if (root == null||root==p||root==q) {
+      return root
+    }
+    const left = lowestCommonAncestor(root.left, p, q);
+    const right = lowestCommonAncestor(root.right,p,q);
+    if (left&&right) {
+      return root
+    }
+    return left?left:right
+    // else if (left){
+    //   return left
+    // }else if(right){
+    //   return right
+    // }
+};
+// @lc code=end
+
