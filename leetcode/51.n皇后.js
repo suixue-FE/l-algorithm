4a173db9a38ac0d5ae71d0fd265af20ef4b40efd
suixuejiqiren <1214587109@qq.com>
2020-09-14 19:08:18 +0800

    回溯-03-n皇后hard

---
+++ leetcode/51.n皇后.js
@@ -0,0 +1,54 @@
+/*
+ * @lc app=leetcode.cn id=51 lang=javascript
+ *
+ * [51] N皇后
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {string[][]}
+ */
+// 确实有点难还得多看看
+var solveNQueens = function(n) {
+  // 1[1,2,3,4]
+  // 2[1,2,3,4]
+  // 3[1,2,3,4]
+  // 4[1,2,3,4]
+// 行不能共存
+// 列不能共存
+// 斜线不能共存（行+列不能一样）+（行-列不能一样）
+  // 一共有n个，证明每一行一个
+  let ret = []
+  find(0)
+  function find(row,item = []){
+    //  第几行
+    if (row===n) {
+      ret.push(
+        item.map(value=> {
+          let str = '';
+            for(let i=0;i<n;i++){
+                if(value == i) str += 'Q';
+                else str += '.'
+            }
+          return str
+        })
+      )
+    }
+    // item.push(row)
+    for (let j =0;j<n;j++){
+      const isCanAdd = item.some((value,index) =>{
+        // index是行，value是列 都是已经存起来的数据
+        return value===j||(value+index)===(j+row)||(value-index)===(j-row)
+      })
+      if (isCanAdd) {
+        continue 
+      }
+      find(row+1,[...item,j])
+    }
+    
+  }
+  return ret
+};
+// @lc code=end
+
--- leetcode/79.单词搜索.js
+++ leetcode/79.单词搜索.js
@@ -10,7 +10,7 @@
  * @param {string} word
  * @return {boolean}
  */
-var exist = function(board, word) {
+var exist1 = function(board, word) {
   // 还有很多可优化的，需要注意，回头看一下
   let str = ''
   let obj = {}
@@ -38,4 +38,35 @@ var exist = function(board, word) {
   }
   return false
 };
+var exist = function(board, word) {
+  // 1、怎么找
+  // 2、什么时候终止
+  // 3、内部什么时候终止找下一步 （缓存中间的过程）
+  if (board.length===0) return false
+  if (word.length===0) return true
+  function find(i,j,cur){
+    if (i>=row||i<0) return false
+    if (j>=col||j<0) return false
+
+    const item = board[i][j]
+    if (item!==word[cur]) return false
+    if (cur==word.length-1) return true
+
+    board[i][j] = null // 当前路径标记为空，为了不让他同一路径查上一步
+    //
+    const ret = find(i,j-1,cur+1)||find(i,j+1,cur+1)||find(i-1,j,cur+1)||find(i+1,j,cur+1)
+    board[i][j] = item
+    return ret
+  }
+  //怎么找
+  const row = board.length
+  const col = board[0].length
+  for (let i = 0; i < row; i++) {
+    for (let j = 0; j < col; j++) {
+      const request = find(i,j,0)
+      if (request) return true
+    }
+  }
+  return false
+}
 // @lc code=end
