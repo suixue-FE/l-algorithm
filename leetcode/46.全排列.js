602a0f524062f89e0f4b44eddda6356bd7d194b5
lisansang <1214587109@qq.com>
2021-01-21 10:08:01 +0800

    复习全排列回溯

--- leetcode/46.全排列.js
+++ leetcode/46.全排列.js
@@ -9,26 +9,47 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
-function backTrace(output,nums,res,used) {
-  // console.log(output);
-  if(output.length === nums.length) {
-    // console.log(output,nums);
-    res.push([...output])
-  }
-  // console.log(res);
-  for (const num of nums) {
-    if(used[num]) continue
-    output.push(num);
-    used[num] = true;
-    backTrace(output,nums,res,used)
-    output.pop()
-    used[num] = false;
-  }
-}
 var permute = function(nums) {
   let results = [] 
-  // 第四个参数是用来处理内部是否已经存在的判定
-  backTrace([],nums,results,{});
+  const backTrace=(res,used)=>{
+    if(res.length === nums.length){
+      results.push(res)
+      return
+    }
+      nums.forEach(val=>{
+        if(!used[val]){
+          res.push(val)
+          used[val] = true
+          backTrace([...res],used)
+          res.pop()
+          used[val] = false
+        }
+      })
+    
+  };
+  backTrace([],{})
   return results
 };
+// function backTrace(output,nums,res,used) {
+//   // console.log(output);
+//   if(output.length === nums.length) {
+//     // console.log(output,nums);
+//     res.push([...output])
+//   }
+//   // console.log(res);
+//   for (const num of nums) {
+//     if(used[num]) continue
+//     output.push(num);
+//     used[num] = true;
+//     backTrace(output,nums,res,used)
+//     output.pop()
+//     used[num] = false;
+//   }
+// }
+// var permute = function(nums) {
+//   let results = [] 
+//   // 第四个参数是用来处理内部是否已经存在的判定
+//   backTrace([],nums,results,{});
+//   return results
+// };
 // @lc code=end
