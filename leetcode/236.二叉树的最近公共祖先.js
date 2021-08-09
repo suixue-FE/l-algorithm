38631096422e01730bf2249dc3ba301c217dd722
lisansang <1214587109@qq.com>
2021-08-09 18:59:19 +0800

    fix: 二叉树祖先

--- leetcode/236.二叉树的最近公共祖先.js
+++ leetcode/236.二叉树的最近公共祖先.js
@@ -18,22 +18,30 @@
  * @param {TreeNode} q
  * @return {TreeNode}
  */
+// var lowestCommonAncestor = function(root, p, q) {
+//     // 这个完全没想出来，看了大佬的半天，试着写一下
+//     if (root == null||root==p||root==q) {
+//       return root
+//     }
+//     const left = lowestCommonAncestor(root.left, p, q);
+//     const right = lowestCommonAncestor(root.right,p,q);
+//     if (left&&right) {
+//       return root
+//     }
+//     return left?left:right
+//     // else if (left){
+//     //   return left
+//     // }else if(right){
+//     //   return right
+//     // }
+// };
 var lowestCommonAncestor = function(root, p, q) {
-    // 这个完全没想出来，看了大佬的半天，试着写一下
-    if (root == null||root==p||root==q) {
-      return root
-    }
-    const left = lowestCommonAncestor(root.left, p, q);
-    const right = lowestCommonAncestor(root.right,p,q);
-    if (left&&right) {
-      return root
-    }
-    return left?left:right
-    // else if (left){
-    //   return left
-    // }else if(right){
-    //   return right
-    // }
-};
+  if (!root) return root
+  if (root===p||root===q) return root
+  const left = lowestCommonAncestor(root.left, p, q)
+  const right = lowestCommonAncestor(root.right, p, q)
+  if (right&&left) return root
+  return left || right
+}
 // @lc code=end
 
